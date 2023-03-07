1027fbde46361aaa756537fee1799910c607b2f9
D-Ronald <Danielronald21@gmail.com>
2023-03-07 17:53:54 -0300

    INSERTION E SELECTION SORT IMPLEMENTADOS

--- ordenacao/algoritms/InsertionSort.java
+++ ordenacao/algoritms/InsertionSort.java
@@ -7,7 +7,18 @@ public class InsertionSort implements Sorter{
     @Override
     public int[] sort(int[] elements) {
         int [] sorted = elements.clone();
-        //TODO Implementar
+        int size =sorted.length;
+        int menor;
+        int pointer = 0;
+        for (int i = 0; i<size;i++){
+            int  key = sorted[i];
+            int j = i-1;
+            while(j>=0 && sorted[j]>key){
+                sorted[j+1] = sorted[j];
+                j = j-1;
+            }
+            sorted[j+1]=key;
+        }
         return sorted;
     }
 
--- ordenacao/algoritms/SelectionSort.java
+++ ordenacao/algoritms/SelectionSort.java
@@ -7,7 +7,20 @@ public class SelectionSort implements Sorter{
     @Override
     public int[] sort(int[] elements) {
         int [] sorted = elements.clone();
-        //TODO Implementar
+        int size =sorted.length;
+        int menor = 0;
+        int pointer = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++) {
+                menor = sorted[i];
+                if (sorted[j] < menor) {
+                    menor = sorted[j];
+                    pointer = sorted[i];
+                    sorted[i] = menor;
+                    sorted[j] = pointer;
+                }
+            }
+        }
         return sorted;
     }
 
